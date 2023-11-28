1304304804c5e298bfb16f7584923dd90b78005f
Tulio Fusco <tuliofusco@hotmail.com>
2023-11-27 21:01:47 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,13 @@ public class Program {
                 if (capturedPiece != null){
                     captured.add(capturedPiece);
                 }
+
+                if (chessMatch.getPromoted() != null){
+                    System.out.print("Enter piece for promotion (B/H/R/Q): ");
+                    sc.nextLine();
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
             }catch (ChessException e){
                 System.out.println(e.getMessage());
                 sc.nextLine();
